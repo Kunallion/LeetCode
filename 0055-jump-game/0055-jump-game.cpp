b072f738a80498d1b3cfc2046d8e5712898b5366
KUNAL SINGH <83157868+Kunallion@users.noreply.github.com>
2023-09-28 21:14:44 +0530

    Time: 50 ms (50.01%), Space: 48.8 MB (21.76%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size()-1;
+        for(int i=nums.size()-2 ; i>=0 ; i--)
+        {
+            if((i+nums[i])>=goal)
+                goal = i;
+        }
+        if(goal==0)
+            return true;
+        return false;
+    }
+};
