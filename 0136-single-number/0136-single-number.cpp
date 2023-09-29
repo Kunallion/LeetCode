6e961118448088572a39968e2821d9a60d7c049c
KUNAL SINGH <83157868+Kunallion@users.noreply.github.com>
2023-09-29 10:53:07 +0530

    Time: 19 ms (35.28%), Space: 17.5 MB (22.42%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int i, ans=0;
+        for(i=0 ; i<nums.size() ; i++)
+        {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
