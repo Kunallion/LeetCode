deb70880784cc7861210783fac6af0c5483685f4
KUNAL SINGH <83157868+Kunallion@users.noreply.github.com>
2023-09-30 11:11:43 +0530

    Time: 8 ms (53.67%), Space: 11.2 MB (5.67%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int i;
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for(i=0 ; i<nums.size() ; i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.second==1)
+            {
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
