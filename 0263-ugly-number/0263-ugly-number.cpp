76a536f7db6fe1509765c91ba6c15db74c6d9b37
KUNAL SINGH <83157868+Kunallion@users.noreply.github.com>
2023-09-30 11:12:16 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (24.10%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        while(true)
+        {
+            if(n<=0)
+                return false;
+            else if(n<=5)
+                return true;
+            else if(n%2==0)
+                n /= 2;
+            else if(n%3==0)
+                n /= 3;
+            else if(n%5==0)
+                n /= 5;
+            else
+                return false;
+        }
+        return true;
+    }
+};
