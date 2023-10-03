bceeb194f388b2e1dde5c3de3e653a4401d25ee9
KUNAL SINGH <83157868+Kunallion@users.noreply.github.com>
2023-10-04 02:31:34 +0530

    Added solution - LeetHub

--- Swap two numbers - GFG/swap-two-numbers.cpp
+++ Swap two numbers - GFG/swap-two-numbers.cpp
@@ -11,7 +11,11 @@ class Solution{
 public:
     pair<int, int> get(int a, int b){
         //complete the function here
-        swap(a, b);
+        
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        
         return {a, b};
     }
 };
