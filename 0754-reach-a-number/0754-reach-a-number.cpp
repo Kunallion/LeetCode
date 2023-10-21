c4a6f0423dc25fa5b1c967222cfcbeb82ad22af0
KUNAL SINGH <83157868+Kunallion@users.noreply.github.com>
2023-10-22 01:46:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.33%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        
+        target = abs(target);
+        int sum=0; 
+        int steps=0;
+
+        while(sum<target){
+            steps++;
+            sum += steps;
+        }
+
+        while((sum-target)%2!=0){
+            steps++;
+            sum += steps;
+        }
+        return steps;
+    }
+};
